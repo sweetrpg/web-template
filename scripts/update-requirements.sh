53b96b3749af7a1599ed021dc5a6b8b66e997c30
Paul Schifferer <paul@schifferers.net>
2025-09-26 08:23:12 -0700

    Updates

--- scripts/update-requirements.sh
+++ scripts/update-requirements.sh
@@ -7,7 +7,10 @@ scriptdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 pushd ${scriptdir}/..
 
 for d in dev docs app tests; do
-    pip-compile requirements/$d.in
+    echo ""
+    echo "----------------------------"
+    echo -e "Requirement: \033[1m${r}\033[0m"
+    pip-compile -r -U requirements/${r}.in
 done
 
 popd
